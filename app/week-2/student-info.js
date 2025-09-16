5d947807f87d8784102553f0f64971c825960322
kawaiimonkey <kawaiimonkeyhuang@gmail.com>
2025-09-16 14:05:46 -0600

    added 2 pages

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Page() { 
+return ( 
+<main> 
+<h1>Name: Licheng</h1> 
+<Link href={"https://github.com/kawaiimonkey/cprg306-assignments"}>https://github.com/kawaiimonkey/cprg306-assignments</Link>
+</main> 
+); 
+} 
